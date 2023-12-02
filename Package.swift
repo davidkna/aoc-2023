ba763e4a74deb2ad7a4231db98e2d470cb864abc
David Knaack <davidkna@users.noreply.github.com>
2023-12-02 12:42:54 +0100

    day 02

--- Package.swift
+++ Package.swift
@@ -5,12 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "aoc-2023",
+    platforms: [
+        .macOS(.v14)
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "day-01",
             path: "Sources/day-01"
+        ),
+        .executableTarget(
+            name: "day-02",
+            path: "Sources/day-02"
         ),
     ]
 )
