f58960499add24b276bc4be06a2507d69362cead
David Knaack <davidkna@users.noreply.github.com>
2023-12-08 10:36:24 +0100

    day 08: shorten gcd impl

--- src/day-08/main.rs
+++ src/day-08/main.rs
@@ -10,9 +10,7 @@ fn greatest_common_divisor(a: u64, b: u64) -> u64 {
     let mut a = a;
     let mut b = b;
     while b != 0 {
-        let t = b;
-        b = a % b;
-        a = t;
+        (a, b) = (b, a % b);
     }
     a
 }
